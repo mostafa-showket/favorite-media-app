ebcbe8b88e2a6edf4a4a8f870412d0f495bb4f7d
Mostafa Khaled Showket <114008599+mostafa-showket@users.noreply.github.com>
2025-08-01 14:40:13 +0300

    udpated migrations

--- favorite-media-server/prisma/migrations/20250730163243_add_genre_field/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE `media` MODIFY `title` VARCHAR(191) NULL,
-    MODIFY `type` VARCHAR(191) NULL,
-    MODIFY `director` VARCHAR(191) NULL,
-    MODIFY `budget` VARCHAR(191) NULL,
-    MODIFY `location` VARCHAR(191) NULL,
-    MODIFY `duration` VARCHAR(191) NULL,
-    MODIFY `yearOrTime` VARCHAR(191) NULL,
-    MODIFY `genre` VARCHAR(191) NULL;
